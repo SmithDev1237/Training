9c0cf6805530fc90b641cd1201544503be5a5aae
Martin Smith <Martin Smith>
2016-02-03 20:57:35 +0000

    Added some unit testing using QUnit.js

---
+++ Unit-Testing/Unit-Testing/Scripts/FizzBuzz.js
@@ -0,0 +1,20 @@
+﻿function fizzBuzz(number) {
+    // Return 'Fizz' if number is divisible by 3.
+    // Return 'Buzz' if number is divisible by 5.
+    // Return the number if not.
+
+    var responseString = "";
+
+    if (number % 3 === 0) {
+        responseString = "Fizz";
+    }
+
+    if (number % 5 === 0) {
+        responseString += "Buzz";
+    }
+
+    if (responseString === "") {
+        responseString = number;
+    }
+    return responseString;
+}
---
+++ Unit-Testing/Unit-Testing/Tests/FizzBuzzTests.js
@@ -0,0 +1,12 @@
+﻿function fizzBuzzTests() {
+    QUnit.test("FizzBuzz Output Test", function (assert) {
+
+        assert.equal(fizzBuzz(0), "FizzBuzz");
+        assert.equal(fizzBuzz(1), 1);
+        assert.equal(fizzBuzz(3), "Fizz");
+        assert.equal(fizzBuzz(5), "Buzz");
+        assert.equal(fizzBuzz(30), "FizzBuzz");
+        assert.equal(fizzBuzz(98), 98);
+        assert.equal(fizzBuzz(), undefined);
+    });
+}
