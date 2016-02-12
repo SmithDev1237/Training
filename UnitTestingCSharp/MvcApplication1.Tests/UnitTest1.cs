61fa5ff634d871a37da995260a8e6a83a74120a6
Martin Smith <smithdev1237@gmail.com>
2016-02-12 10:39:02 +0000

    Addded some c# unit testing

---
+++ UnitTestingCSharp/MvcApplication1.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MvcApplication1.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void EvenTest()
+        {
+            var test = new Helpers.Test();
+            var result = test.isEven(2);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void OddTest()
+        {
+            var test = new Helpers.Test();
+            var result = test.isEven(1);
+            Assert.AreEqual(false, result);
+            
+        }        
+    }
+}
---
+++ UnitTestingCSharp/MvcApplication1/Helpers/Test.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication1.Helpers
+{
+    public class Test
+    {
+        public bool isEven(int number)
+        {
+            if(number % 2 == 0){
+
+                return true;
+            }
+            else{
+                return false;
+            }
+            
+        }
+    }
+}
